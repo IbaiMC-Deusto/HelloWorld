7a06cab2e76112e1742c90062dea4ea1d915fdb7
IbaiMC-Deusto <ibai.mellado@opendeusto.es>
2026-03-12 09:05:21 +0100

    Movement Script updated
    Added movement options for all directions

--- Prueba/Assets/Scripts/Movement.cs
+++ Prueba/Assets/Scripts/Movement.cs
@@ -15,13 +15,41 @@ public class Movement : MonoBehaviour
     }
 
     /// <summary>
-    /// If <b> space </b> key is pressed, the object moves forward
+    /// Checks the key presses by the player and moves the object.
+    /// 
+    /// <para name = "Space"> Space -> Forward </para>
+    /// <para name = "LeftControl"> LeftControl -> Back </para>
+    /// <para name = "RigthArrow"> RightArrow -> Right </para>
+    /// <para name = "LeftArrow"> LeftArrow -> Left </para>
+    /// <para name = "DownArrow"> DownArrow -> Down </para>
+    /// <para name = "UpArrow"> UpArrow -> Up </para>
     /// </summary>
+
     void Momevent()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             this.transform.position += Vector3.forward * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            this.transform.position += Vector3.back * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            this.transform.position += Vector3.right * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            this.transform.position += Vector3.left * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            this.transform.position += Vector3.down * Time.deltaTime;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            this.transform.position += Vector3.up * Time.deltaTime;
         }
     }
 }
