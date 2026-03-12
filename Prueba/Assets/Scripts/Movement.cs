34e22abd5547be39e0dce711e5daf7bbb3ec601d
IbaiMC-Deusto <ibai.mellado@opendeusto.es>
2026-03-12 08:42:42 +0100

    Momevent Script created and added

---
+++ Prueba/Assets/Scripts/Movement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Momevent();
+    }
+
+    /// <summary>
+    /// If <b> space </b> key is pressed, the object moves forward
+    /// </summary>
+    void Momevent()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            this.transform.position += Vector3.forward;
+        }
+    }
+}
